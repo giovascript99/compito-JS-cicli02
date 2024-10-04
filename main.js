39bef03b106e46670972c642893f2e341a601c83
Giovanni Perniola <gioperniola1999@gmail.com>
2024-10-04 20:20:45 +0200

    selfwork corretto

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+let accumulator = 0;
+let counter = 0;
+
 for(let i = 1; i <= 20; i++){
-    if(i % 2 === 0){
+    if(i % 2 == 0){
     console.log(i);
     
     }else{
-        console.log();
+        accumulator = accumulator + i
+        counter ++
         
     }
 }
+
+console.log(`accumulator = ${accumulator}`);
+console.log(`counter = ${counter}`);
+console.log(`risultato media = ${accumulator/counter}`);
+
+
